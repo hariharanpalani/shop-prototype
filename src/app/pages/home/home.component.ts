b4157cc0e83f0be735bd6ff5267c4d86e3c1d532
Hariharan Palani <hariharanpalani@live.com>
2018-12-02 03:34:14 +0530

    updated the landing page aimge

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,7 +4,7 @@ import { Product } from "../../app.models";
 
 @Component({
   selector: 'app-home',
-  templateUrl: '/home.component.html',
+  templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
