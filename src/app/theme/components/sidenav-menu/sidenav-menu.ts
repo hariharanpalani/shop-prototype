53fdc3e97589859846591ac024b8e1058f3c4f68
Hariharan Palani <hariharanpalani@live.com>
2018-12-01 19:39:40 +0530

    first commit

---
+++ src/app/theme/components/sidenav-menu/sidenav-menu.ts
@@ -0,0 +1,13 @@
+import { SidenavMenu } from './sidenav-menu.model';
+
+export const sidenavMenuItems = [ 
+    new SidenavMenu (1, 'Home', '/', null, null, false, 0),
+    new SidenavMenu (2, 'RFQ', null, '/', null, false, 0), 
+    new SidenavMenu (3, 'Quotes', null, '/', null, false, 0), 
+    new SidenavMenu (4, 'Notifications', null, '/', null, false, 0),
+    new SidenavMenu (5, 'Manage / Setup Site', null, '/', null, false, 0),
+    new SidenavMenu (6, 'Networks', null, '/', null, false, 0),
+    new SidenavMenu (7, 'Tools', null, '/', null, false, 0),
+    new SidenavMenu (8, 'Products', null, '/', null, false, 0),
+    new SidenavMenu (9, 'Promotions', null, '/', null, false, 0)
+]
