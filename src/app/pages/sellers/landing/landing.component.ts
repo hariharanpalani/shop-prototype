5568ab9e93e634a9995d529c86f6b9c2c943f52e
Kirubaharan Palani <kirubap@cadonix.com>
2018-12-02 02:30:27 +0530

    modified seller page

--- src/app/pages/sellers/landing/landing.component.ts
+++ src/app/pages/sellers/landing/landing.component.ts
@@ -4,9 +4,16 @@ import { FormGroup } from "@angular/forms";
 
 @Component({
     selector: 'app-landing',
-    templateUrl: './landing.component.html'
+    templateUrl: './landing.component.html',
+    styleUrls: ['landing.component.css'],
 })
+
+@Component({
+    selector: 'app-landing',
+    templateUrl: './landing.component.html',
+    styleUrls: ['landing.component.css'],
+})
+
 export class LandingComponent {
     loginForm: FormGroup;
-
 }
